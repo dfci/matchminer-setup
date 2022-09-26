8563319f68ebad6ec7727666e8b7fc1bea03e240
Jason Hansel <jason@jasonhansel.com>
2022-09-26 11:24:44 -0400

    separate setup and install scripts

--- setup.sh
+++ setup.sh
@@ -1,81 +1,99 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! which docker >/dev/null
+
+if test -z "${BASH_VERSION:-}"
 then
-  echo 'this script requires docker'
+  echo 'this script must be run using bash'
   exit 1
 fi
 
-ERRS="$(docker info --format '{{range .ServerErrors}}{{.}}{{end}}' 2>&1)"
-
-if ! test -z "$ERRS"
+if ! which git >/dev/null
 then
-  echo 'cannot connect to docker:'
-  echo "$ERRS"
+  echo 'this script requires git'
   exit 1
 fi
 
-echo "Setting up docker network..."
-docker network create mm_custom_network
+declare -a GIT_REPOS=(
+  'https://github.com/dfci/matchminer-setup.gitxx' # FIXME
+  'git@github.com:dfci/matchminer-setup.git'
+)
 
-echo "Setting up mongo db..."
-docker run -d --name mongo \
-              --network=mm_custom_network \
-              mongo:3.6.14 mongod --smallfiles --replSet=rs0
+export GIT_TERMINAL_PROMPT=0
 
-echo "Initializing mongodb as replica set..."
-while ! docker exec mongo mongo matchminer --eval "rs.initiate();" >/dev/null 2>/dev/null
+# You can set the variable MATCHMINER_GIT_REMOTE_REF to check out a different branch:
+GIT_REMOTE_REF="${MATCHMINER_GIT_REMOTE_REF:-origin/main}"
+
+# First, see if setup.sh is being run from a clone of the "matchminer-setup" repository:
+LOCAL_SETUP_DIR=""
+if test -n "${BASH_SOURCE[0]:-}"
+then
+  SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+  pushd $SCRIPT_DIR >/dev/null
+  if git remote >/dev/null 2>/dev/null
+  then
+    LOCAL_GIT_REPOS="$(git remote | xargs -I'{}' git remote get-url --all '{}')"
+    while IFS= read -r LOCAL_REPO; do
+      for REMOTE_REPO in "${GIT_REPOS[@]}"
+      do
+        if test  "$LOCAL_REPO" '=' "$REMOTE_REPO"
+        then
+          LOCAL_SETUP_DIR="$SCRIPT_DIR"
+        fi
+      done
+    done <<< "$LOCAL_GIT_REPOS"
+  fi
+  popd >/dev/null
+fi
+
+# If so, just run the "install" script in that local clone:
+if test -n "$LOCAL_SETUP_DIR"
+then
+  echo "Entering local copy of matchminer-setup repository at: $LOCAL_SETUP_DIR"
+  cd "$LOCAL_SETUP_DIR"
+  echo 'Running install script'
+  exec ./install
+fi
+
+# Otherwise, we try to clone the repository from GitHub, using various protocols:
+
+echo "Cloning matchminer-setup repository"
+CLONE_DIR=""
+for REMOTE_REPO in "${GIT_REPOS[@]}"
 do
-  echo 'Waiting for mongodb to start...'
-  sleep 1
+  echo "Attempting to clone from: $REMOTE_REPO"
+  TEMP_DIR="$(mktemp -d)"
+  echo "Will clone into: $TEMP_DIR"
+  CLONE_ATTEMPT_DIR="$TEMP_DIR/matchminer-setup"
+  mkdir "$CLONE_ATTEMPT_DIR"
+  pushd "$CLONE_ATTEMPT_DIR" > /dev/null
+  # This roughly follows https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  git init -q
+  git config remote.origin.url "$REMOTE_REPO"
+  git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+  git config core.autocrlf false
+  if git fetch --force origin >/dev/null 2>/dev/null
+  then
+    CLONE_DIR="$CLONE_ATTEMPT_DIR"
+    popd > /dev/null
+    break
+  else
+    echo "Clone attempt failed"
+    rm -rf "$TEMP_DIR"
+    popd > /dev/null
+  fi
 done
+if test -z "$CLONE_DIR"
+then
+  echo "Failed to clone repository!"
+  exit 1
+fi
 
-echo "Setting up elasticsearch..."
-docker pull matchminer/mmelastic:latest
-docker run -d --name=mm_elastic \
-              --network=mm_custom_network \
-              matchminer/mmelastic:latest
 
-echo "Setting up api..."
-docker pull matchminer/mm_api:latest
-docker run -d --name=mm_api \
-              --network=mm_custom_network \
-              --env=SECRETS_JSON=/var/www/apache-flask/api/SECRETS_JSON.json \
-              --env=MONGO_URI=mongodb://mongo:27017/matchminer \
-              matchminer/mm_api:latest python pymm_run.py serve --no-auth
-
-echo "Setting up mongo-connector..."
-docker pull matchminer/mm_connector:latest
-docker run -d --name=mm_connector \
-              --network=mm_custom_network \
-              --env MONGO_URI=mongodb://mongo:27017/matchminer \
-              --env ELASTICSEARCH=mm_elastic \
-              --env ELASTIC_INDEX=matchminer.trial \
-              matchminer/mm_connector:latest
-
-echo "Setting up UI..."
-docker pull matchminer/mm_ui:demo
-docker run -d --name=mm_ui \
-              --network=mm_custom_network \
-              matchminer/mm_ui:latest
-
-echo "Setting up nginx..."
-docker pull matchminer/mm_nginx:latest
-docker run -d --network=mm_custom_network \
-              --name=nginx \
-              -p 80:8881 matchminer/mm_nginx
-
-echo "Downloading demo data..."
-EXTRACT_DIR="$(mktemp -d)"
-pushd $EXTRACT_DIR > /dev/null
-curl -fsSL -o matchminer.tar.gz https://raw.githubusercontent.com/dfci/matchminer-setup/HEAD/matchminer.tar.gz
-tar -xzvf matchminer.tar.gz
-docker cp matchminer mongo:/
-popd
-rm -rf $EXTRACT_DIR
-
-echo "Loading demo data into mongo..."
-docker exec mongo mongorestore --quiet matchminer
-
-echo "SUCCESS. To view, navigate to http://localhost"
+# If the clone succeeds, we can run the install script:
+echo "Entering remote copy of matchminer-setup repository at: $CLONE_DIR"
+cd "$CLONE_DIR"
+echo "Checking out reference: $GIT_REMOTE_REF"
+git reset --quiet --hard "$GIT_REMOTE_REF"
+echo "Running install script..."
+exec ./install
